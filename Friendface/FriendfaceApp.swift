8feaec5e17db15e82a2738643c9deb9a0fa4737e
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-12-10 14:45:33 +0300

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Dmitry Sharabin on 10.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
